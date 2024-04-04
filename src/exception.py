50a914ba5e31cdc6cdd9140578eb21e27a33a534
Aarohi Dharmadhikari <aarohi.july@gmail.com>
2024-04-04 22:51:21 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -22,11 +22,3 @@ class CustomException(Exception):
     
 
 
-if __name__ =="__main__":
-
-    try:
-        a=1/0
-
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise CustomException(e,sys)
